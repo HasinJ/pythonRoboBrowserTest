b32aa0be3a69cb325ef6651545d2c003ee3d9c45
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-03-27 02:02:50 -0400

    grabs login form

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import re
-
+from bs4 import BeautifulSoup
 from robobrowser import RoboBrowser
+browser = RoboBrowser(parser='html.parser')
+browser.open('https://adqsr.radiantenterprise.com/bin/orf.dll/PE.platformForms.login.select.1.ghtm')
+
+login_form = browser.get_form(id='theForm')
+
+#tells us what HTML we're dealing with
+print(login_form.parsed)
