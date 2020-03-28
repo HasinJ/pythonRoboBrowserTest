b3486f26387301454e5c0925c07a0907044f894d
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-03-28 18:34:58 -0400

    goes to date, selects default, then runs report

--- main.py
+++ main.py
@@ -60,6 +60,87 @@ time.sleep(1)
 orgUnit.send_keys(Keys.ENTER)
 time.sleep(1)
 
+busUnit = driver.find_element_by_id('__lufBusUnit')
+busUnit.clear()
+time.sleep(1)
+busUnit.send_keys('347884')
+busUnit.send_keys(Keys.ENTER)
+
+dateUnit = driver.find_element_by_id('lkupDates')
+dateUnit.clear()
+dateUnit.send_keys('text to prompt dialog box')
+time.sleep(1)
+dateUnit.send_keys(Keys.ENTER)
+time.sleep(3)
+
+
+
+main_page = driver.current_window_handle
+
+handles = driver.window_handles
+
+# print the window_handle length
+print(len(handles))
+
+popup_window_handle = None
+# loop through the window handles and find the popup window.
+for handle in driver.window_handles:
+    if handle != main_page:
+        print(handle)
+        popup_window_handle = handle
+        break
+# switch to the popup window.
+driver.switch_to.window(popup_window_handle)
+
+#frame switch to date frame
+wait = WebDriverWait(driver,10)
+frame = wait.until(EC.presence_of_element_located((By.ID, "renderFrame")))
+driver.switch_to.frame(frame)
+
+#click on save and close
+wait = WebDriverWait(driver,10)
+saveANDclose = wait.until(EC.presence_of_element_located((By.ID, 'waSaveClose')))
+saveANDclose.send_keys(Keys.ENTER)
+
+#go back to previous screen/frame
+driver.switch_to.window(main_page)
+driver.switch_to.default_content()
+
+wait = WebDriverWait(driver,30)
+frame = wait.until(EC.presence_of_element_located((By.ID, "fraContent")))
+driver.switch_to.frame(frame)
+
+wait = WebDriverWait(driver,30)
+frame = wait.until(EC.presence_of_element_located((By.ID, "Frame2")))
+driver.switch_to.frame(frame)
+
+wait = WebDriverWait(driver,10)
+submit = wait.until(EC.presence_of_element_located((By.ID, 'wrLHSalesMixCon__AutoRunReport')))
+submit.send_keys(Keys.ENTER)
+
+print(driver.page_source)
+#WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@class='btn btn-primary' and @id='YesBtn']")))
+
+
+#submit = driver.find_element_by_id('wrLHSalesMixCon__AutoRunReport')
+#ActionChains(driver).move_to_element(submit).click(submit).perform()
+#time.sleep(1)
+
+#print(driver.window_handles)
+#actions = ActionChains(driver)
+#actions.send_keys(Keys.TAB).perform()
+#time.sleep(1)
+
+#wait = WebDriverWait(driver,30)
+#time.sleep(5)
+#print(driver.page_source)
+#frame = wait.until(EC.presence_of_element_located((By.ID, "renderFrame")))
+#driver.switch_to.frame(frame)
+#actions.send_keys(Keys.ENTER).perform()
+#time.sleep(1)
+#actions.move_to_element(submit).click(submit).perform()
+
+
 time.sleep(5)
 print(driver.page_source)
 
