5e75d200d865068fbf89a2ec9fc4cb41bc285f50
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-03-28 05:49:15 -0400

    goes to product mix sales

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
 import config
 driver = webdriver.Ie(r"H:\IEDriver\IEDriverServer.exe")
+wait = WebDriverWait(driver,5)
 
 driver.get('https://adqsr.radiantenterprise.com/bin/orf.dll/PE.platformForms.login.select.1.ghtm')
 
@@ -17,3 +24,24 @@ continueBtn.send_keys(Keys.ENTER)
 
 continueBtn = driver.find_element_by_id('waContinue')
 continueBtn.send_keys(Keys.ENTER)
+
+#frame = driver.find_element_by_id('MenuFrame')
+frame = wait.until(EC.presence_of_element_located((By.ID, "MenuFrame")))
+driver.switch_to.frame(frame)
+
+wait = WebDriverWait(driver,60)
+#keyReports = driver.find_element_by_id('Node_1004552_')Node_1004555_ Node_1018704_0
+#keyReports.send_keys(Keys.ENTER)
+
+print(driver.page_source)
+
+keyReports = wait.until(EC.presence_of_element_located((By.ID, "Node_1018703_0")))
+
+ActionChains(driver).move_to_element(keyReports).click(keyReports).perform()
+
+print(driver.page_source)
+
+
+
+
+#window.top.ShowMenu(!window.top.bMenuVisible);
