2e0289ed77e226319f272e7ac8fc48742971a2e0
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-04-01 04:39:32 -0400

    properly clicks an element in unit modal

--- main.py
+++ main.py
@@ -68,11 +68,12 @@ continueBtn = driver.find_element_by_id('waContinue')
 continueBtn.send_keys(Keys.ENTER)
 
 #frame = driver.find_element_by_id('MenuFrame')
+time.sleep(3)
 frame = wait.until(EC.presence_of_element_located((By.ID, "MenuFrame")))
 driver.switch_to.frame(frame)
 
 wait = WebDriverWait(driver,waitTime)
-productMixReport = wait.until(EC.element_to_be_clickable((By.ID, "Node_1018703_0")))
+productMixReport = wait.until(EC.element_to_be_clickable((By.ID, "Node_1018704_0")))
 ActionChains(driver).move_to_element(productMixReport).click(productMixReport).perform()
 
 driver.switch_to.default_content()
@@ -128,7 +129,14 @@ if len(rows) == len(pcNumbers):
     print('PC numbers parsed and scraped')
     time.sleep(1)
 
+even = driver.find_elements_by_class_name('gridRowEven')
+odd = driver.find_elements_by_class_name('gridRowOdd')
+#test = wait.until(EC.element_to_be_clickable((By.ID, f"{even[0]}")))
+#print(even[0])
 
+time.sleep(2)
+print(even[0].find_element_by_class_name('gridCell').find_element_by_tag_name('span').get_attribute("innerHTML"))
+ActionChains(driver).move_to_element(even[2]).click(even[2]).perform()
 
 time.sleep(3)
 driver.quit()
