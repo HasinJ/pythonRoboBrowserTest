b64d80b6e7f228a65a753724b99158698fbae571
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-04-07 19:20:31 -0400

    datetime stuff happens here now

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import datetime
+dateToday = datetime.datetime.now().strftime('%x') #local version of date
+year = datetime.datetime.now().strftime('%Y')
+dateToday = dateToday[:6] + year #adds the year in full, "2021" instead of "21"
+dateToday = '04/02/2020' #temp
+dateDotNotation = dateToday.replace('/', '.')
+print(dateToday)
+
 radUser = ''
 radPass = ''
